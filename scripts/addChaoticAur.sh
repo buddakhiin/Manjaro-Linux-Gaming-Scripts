77b109a1e16850e4784cb779f94bc66252574d67
Budda Pellerin-Bidalot <buddakhiin.pro@gmail.com>
2024-07-01 20:33:04 +0200

    add sudo in the Chaotic AUR setup script in order to not have to launch it with sudo

--- scripts/addChaoticAur.sh
+++ scripts/addChaoticAur.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key FBA220DFC880C036
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-echo "[chaotic-aur]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
-pacman -S powerpill paru yay
-pacman -Sy && powerpill -Su && paru -Su
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key FBA220DFC880C036
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo echo "[chaotic-aur]" >> /etc/pacman.conf
+sudo echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+sudo pacman -S powerpill paru yay
+sudo pacman -Sy && powerpill -Su && paru -Su
